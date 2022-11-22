20ad46011720a0dc569877c085fc0ae7fc89e1b2
GARNIER-GULINO Enzo <z84deux@gmail.com>
2022-11-22 18:17:29 +0100

    Python file + getAbstract method

--- TestLangage.py
+++ TestLangage.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 import subprocess
 
-filename = "ACL2004 HEADLINE.pdf"
+filename = "ACL2004-HEADLINE.pdf"
+path = "../Corpus/"
 fileToWrite = "SD/texte.txt"
 
-def writeFilenameInTxt(fileToWrite, filename):
-    with open(fileToWrite, 'w') as f:
-        f.write(filename)
+def initializeTxt(fileToCreate):
+    f = open(fileToCreate, 'w').close()
+
+def writeInTxt(fileToWrite, whatToWrite):
+    with open(fileToWrite, 'a') as f:
+        f.write(whatToWrite)
+        f.write("\n")
 
 def convertPdfFileToTxt(filename):
     filenameWithoutExtensionArray = filename.split(".")
@@ -16,27 +21,72 @@ def convertPdfFileToTxt(filename):
     filenameWithoutExtension = filenameWithoutExtension.replace(" ", "_")
 
     # On exécute la commande bash pour convertir
-    subprocess.run(["pdftotext", filename, filenameWithoutExtension + ".txt"])
+    subprocess.run(["pdftotext", path + filename, path + filenameWithoutExtension + ".txt"])
 
 def readTxtFile(filename):
-    file = open(filename,"r")
-    lines = file.readlines()
+    f = open(path + filename,"r")
+    lines = f.readlines()
 
     # On affiche la 1ère ligne
     print(lines[0])
 
+def getAbstract(filename):
+    f = open(path + filename,"r")
+    lines = f.readlines()
+    content = ""
+    vide = False
+    
+    ## lines est un tableau qui contient tout le texte
+    for i in range(0, len(lines)):
+        
+        
+        linesTable = lines[i].split(" ")
+        if (linesTable[0] == "Abstract") or (linesTable[0] == "Abstract\n") or (linesTable[0] == "ABSTRACT") or (linesTable[0] == "ABSTRACT\n"):
+            content = ""
+
+        if vide == True:
+            if (linesTable[0] == "Introduction") or (linesTable[0] == "Introduction\n") or (linesTable[0] == "INTRODUCTION") or (linesTable[0] == "INTRODUCTION\n") or (linesTable[0] == "1") or (linesTable[0] == "1\n") or (linesTable[0] == "I."):
+                return content
+            if (len(linesTable) > 1):
+                if (linesTable[1] == "Introduction") or (linesTable[1] == "Introduction\n") or (linesTable[1] == "INTRODUCTION\n") or (linesTable[1] == "INTRODUCTION\n") or (linesTable[1] == "1") or (linesTable[1] == "1\n") or (linesTable[1] == "I."):
+                    return content
+            else:
+                content = ""
+                vide = False
+
+        # Si la ligne est vide
+        if lines[i] == "\n":
+            vide = True
+        else:
+            content = content + lines[i]
+
 # Main 
 def launch(filename):
-    filenameTxtArray = filename.split(".")
+
+    # On vide le .txt
+    initializeTxt(fileToWrite)
+
+    filenameTxtArray = filename.split(".pdf")
     filenameTxt = filenameTxtArray[0] + ".txt"
 
     # filenameTxt = nom du fichier avec _ à la place des espaces
     filenameTxt = filenameTxt.replace(" ", "_")
 
-    # Appel des fonctions
+    # Appel des fonctions :
+    # Conversion du pdf en txt
     convertPdfFileToTxt(filename)
-    readTxtFile(filenameTxt)
-    writeFilenameInTxt(fileToWrite, filenameTxt)
+
+    # Écriture du nom du fichier pdf
+    writeInTxt(fileToWrite, filenameTxt)
+
+    # Écriture de l'abstract
+    abstract = getAbstract(filenameTxt)
+
+    if abstract:
+        print(abstract)
+        writeInTxt(fileToWrite, abstract)
+    else:
+        print("Il n'y a pas d'abstract dans ce document")
 
 # Main
 launch(filename)
