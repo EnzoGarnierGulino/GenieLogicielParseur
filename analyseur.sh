b3d9bf425eb5f701dc4d892d1e13811541fbb47a
Ange Curé <ange.cure84@gmail.com>
2022-11-23 15:53:46 +0100

    Modification du programme bash pour accepter l'argument -x -t

--- analyseur.sh
+++ analyseur.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-rm -rf "$1_txt"
-mkdir "$1_txt"
-for f in $1*.pdf
-do
-    nom_origin=$(echo "$f" | grep -o "[^/]*$" | cut -d'.' -f1)
-    nom_dest="$1_txt/$nom_origin.txt"
-    touch $nom_dest
-    python3 analyseur.py $1 $nom_origin $nom_dest
-done
+type_sortie=$1
+dossier_origine=$2
+
+if [[ ! -z $dossier_origine && $type_sortie == "-t" || $type_sortie == "-x" ]]
+then
+    rm -rf "$dossier_origine/_txt"
+    mkdir "$dossier_origine/_txt"
+    for f in $dossier_origine*.pdf
+    do
+        nom_origin=$(echo "$f" | grep -o "[^/]*$" | cut -d'.' -f1)
+        if [ $type_sortie == "-t" ]
+        then
+            nom_dest="$dossier_origine/_txt/$nom_origin.txt"
+        else
+            nom_dest="$dossier_origine/_txt/$nom_origin.xml"
+        fi
+        touch $nom_dest
+        python3 analyseur.py $dossier_origine $nom_origin $nom_dest $type_sortie
+    done
+else
+    echo "Erreur au niveau des arguments veillez entre -t ou -x en premier argument et le dossier en deuxieme argument"
+fi
